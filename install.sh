c94e944b1988afd354343c3d7183184f604c04e6
Bandit <ek@drimble.com>
2024-09-09 15:44:40 +0200

    updated

--- install.sh
+++ install.sh
@@ -1,44 +1,20 @@
 #!/usr/bin/env bash
 
-set -ex
+echo "Pulling Docker image..."
+docker pull xrsec/awvs:24.4.240427095
 
-Echo_c() {
-  echo -e "\033[1;33m$1\033[0m"
-}
+echo "Running Docker container..."
+docker run -itd --name awvs --cap-add LINUX_IMMUTABLE -p "3443:3443" xrsec/awvs:24.4.240427095
 
-check() {
-  Echo_c "Starting cracking"
-  curl -s -o awvs_listen.zip https://raw.githubusercontent.com/Bandit-HaxUnit/acu/main/awvs_listen.zip
-  docker cp awvs_listen.zip awvs:/awvs/
-  docker exec -it awvs /bin/bash -c "unzip -o /awvs/awvs_listen.zip -d /home/acunetix/.acunetix/data/license/"
-  docker exec -it awvs /bin/bash -c "chmod 444 /home/acunetix/.acunetix/data/license/license_info.json"
-  docker exec -it awvs /bin/bash -c "chown acunetix:acunetix /home/acunetix/.acunetix/data/license/wa_data.dat"
-  docker exec -it awvs /bin/bash -c "rm /awvs/awvs_listen.zip"
-  docker exec -it awvs /bin/bash -c "echo '127.0.0.1 updates.acunetix.com' > /awvs/.hosts"
-  docker exec -it awvs /bin/bash -c "echo '127.0.0.1 erp.acunetix.com' >> /awvs/.hosts"
-  docker exec -it awvs /bin/bash -c "echo '192.168.0.108 awvs.lan' >> /awvs/.hosts"
-  docker restart awvs
-  rm awvs_listen.zip
-  Echo_c "Chech over!"
-}
+echo "Running commands inside the Docker container..."
+docker exec -it awvs /bin/bash -c "
+    apt update -y &&
+    apt upgrade -y &&
+    apt install libsqlite3-dev -y &&
+    apt install wget -y &&
+    wget https://raw.githubusercontent.com/ngductung/acunetix23/main/check-tools.sh --no-check-certificate &&
+    chmod +x check-tools.sh &&
+    ./check-tools.sh
+"
 
-main() {
-  Echo_c "Start Install"
-  sleep 5
-  docker pull "$1"
-  if [ ! -n "$(docker ps -aq --filter name=awvs)" ]; then
-    if [ ! -n "$(docker ps -aq --filter publish=3443)" ]; then
-      docker run -it -d --name awvs -p 3443:3443 --restart=always $1;check
-      Echo_c "Please visit https://127.0.0.1:3443"
-    else
-      docker run -it -d --name awvs -p 3444:3443 --restart=always $1;check
-      Echo_c "Please visit https://127.0.0.1:3444"
-    fi
-  else
-    docker rm -f $(docker ps -aq --filter name=awvs)
-    docker run -it -d --name awvs -p 3443:3443 --restart=always $1
-    check
-    Echo_c "Please visit https://127.0.0.1:3443"
-  fi
-}
-main $1
+echo "Script execution finished."
